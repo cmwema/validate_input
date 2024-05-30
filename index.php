c0d2f7192510ebdb28a008f22c166b1e0e05e724
cmwema <calebmwema65@gmail.com>
2024-05-30 15:53:19 +0300

    initial commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css" />
+    <title>Document</title>
+</head>
+
+<body>
+    <form>
+        <fieldset>
+            <legend>Payment Form</legend>
+            <div class="form-row">
+                <label for="phone_no">Phone no.</label>
+                <input id="phone_no" type="tel" />
+            </div>
+        </fieldset>
+    </form>
+</body>
+
+</html>
